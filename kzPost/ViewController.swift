09a31a2ec16d62fd590b0077256f64ed4f53bc96
Dosbol Duysekov <dosbol98@gmail.com>
2017-06-12 14:27:14 +0600

    Initial Commit

---
+++ kzPost/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kzPost
+//
+//  Created by Dosbol Duysekov on 6/12/17.
+//  Copyright © 2017 Dosbol Duysekov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
